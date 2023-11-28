a6a2d82bc60cac0bf7717e07996d09f4d3a2c273
Ahmad Ibrahim <ahmad.ibrahim@spectrocloud.com>
2023-11-28 13:43:39 -0800

    test: add generateRef unit test

--- internal/validators/oci_validator_test.go
+++ internal/validators/oci_validator_test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/spectrocloud-labs/validator-plugin-oci/api/v1alpha1"
+	"github.com/spectrocloud-labs/validator/pkg/types"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,6 +15,12 @@ const (
 	longURL   = "745150053801.dkr.ecr.us-east-1.amazonaws.com.invalid"
 	shortURL  = "dkr.ecr.us-east-1.amazonaws.com"
 	notEcrURL = "745150053801.dkr.notEcr.us-east-1.amazonaws.com"
+
+	registry = "registry"
+)
+
+var (
+	vr = buildValidationResult(v1alpha1.OciRegistryRule{})
 )
 
 func TestParseEcrRegion(t *testing.T) {
@@ -52,6 +60,58 @@ func TestParseEcrRegion(t *testing.T) {
 		if tc.expectedErr != nil {
 			assert.EqualError(t, err, tc.expectedErr.Error())
 		} else {
+			assert.NoError(t, err)
+		}
+	}
+}
+
+func TestGenerateRef(t *testing.T) {
+	type testCase struct {
+		registry         string
+		artifact         string
+		validationResult *types.ValidationResult
+		expectedRefName  string
+		expectErr        bool
+	}
+
+	testCases := []testCase{
+		{
+			registry:         registry,
+			artifact:         "artifact@sha256:ddbac6e7732bf90a4e674a01bf279ce27ea8691530b8d209e6fe77477e0fa406",
+			validationResult: vr,
+			expectedRefName:  "registry/artifact@sha256:ddbac6e7732bf90a4e674a01bf279ce27ea8691530b8d209e6fe77477e0fa406",
+			expectErr:        false,
+		},
+		{
+			registry:         registry,
+			artifact:         "artifact:v1.0.0",
+			validationResult: vr,
+			expectedRefName:  "registry/artifact:v1.0.0",
+			expectErr:        false,
+		},
+		{
+			registry:         registry,
+			artifact:         "artifact",
+			validationResult: vr,
+			expectedRefName:  "registry/artifact:latest",
+			expectErr:        false,
+		},
+		{
+			registry:         registry,
+			artifact:         "invalidArtifact",
+			validationResult: vr,
+			expectedRefName:  "",
+			expectErr:        true,
+		},
+	}
+
+	for _, tc := range testCases {
+		ref, err := generateRef(tc.registry, tc.artifact, tc.validationResult)
+
+		if tc.expectErr {
+			assert.NotNil(t, err)
+		} else {
+			assert.Contains(t, ref.Name(), tc.expectedRefName)
 			assert.NoError(t, err)
 		}
 	}
