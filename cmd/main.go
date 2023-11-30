56369a24fd87be1917e47701421c5146eec7e121
Ahmad Ibrahim <ahmad.ibrahim@spectrocloud.com>
2023-11-30 09:46:33 -0800

    fix: update leader election id

--- cmd/main.go
+++ cmd/main.go
@@ -68,7 +68,7 @@ func main() {
 		Scheme:                 scheme,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "1f172fb1.spectrocloud.labs",
+		LeaderElectionID:       "ecaf1259.spectrocloud.labs",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
