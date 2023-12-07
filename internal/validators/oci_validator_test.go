eb6f8d744dbbdfcef07296604c2457f753925040
Ahmad Ibrahim <ahmad.ibrahim@spectrocloud.com>
2023-12-07 11:26:16 -0800

    test: add TestSetupTransportOpts

--- internal/validators/oci_validator_test.go
+++ internal/validators/oci_validator_test.go
@@ -3,8 +3,10 @@ package validators
 import (
 	"errors"
 	"fmt"
+	"net/http"
 	"testing"
 
+	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/spectrocloud-labs/validator-plugin-oci/api/v1alpha1"
 	"github.com/spectrocloud-labs/validator/pkg/types"
 	"github.com/stretchr/testify/assert"
@@ -17,6 +19,8 @@ const (
 	notEcrURL = "745150053801.dkr.notEcr.us-east-1.amazonaws.com"
 
 	registry = "registry"
+
+	caCert = "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURGRENDQWZ5Z0F3SUJBZ0lSQU1IM2F5UWUvYUkwK1Y0OGE0QnlNYVF3RFFZSktvWklodmNOQVFFTEJRQXcKRkRFU01CQUdBMVVFQXhNSmFHRnlZbTl5TFdOaE1CNFhEVEl6TURneE9UQXdNRGMwTjFvWERUSTBNRGd4T0RBdwpNRGMwTjFvd0ZERVNNQkFHQTFVRUF4TUphR0Z5WW05eUxXTmhNSUlCSWpBTkJna3Foa2lHOXcwQkFRRUZBQU9DCkFROEFNSUlCQ2dLQ0FRRUFtSTlaOUp4Zmg1SlZ0REMyS1U3WS85K0srSzFGanI1T3ZOUmk1RE1iR3NpZ2t6N2wKR054ZkgwSWdJRFdPZ1Q5L3YvNTJ5N1NZcnNrYWJYRVR1TEs3ajlaTXdXck9ZZm1mckcva1VMK3FlTThPYjZZdQorSUhNV3E4Z3VOdzJ2UW9yK214eW1JRUFTc3ZsTDBzd25vSXVQWk1GbFg5NEpWNUJtR3BtVjFrNmZaSVh2b05nClVUaHFoSE4vUFVIVDNibkxYaGlTdFNCZjBIMFR1U3BLMitEVXpvOFVRdlNvaStyV0k5SXRRRENZemtrWjg0bjIKeEp6WCtHSXlvYjNsdGdXU3ZSYmRURU9VK1pmYm0xVTRMV1U4YjdhVWRZSVdwM1EzSEVZK2F1WG1SbmlRSld2aQpQVUJrNTBUQnVPNFFJSWx0VGtHS3VTM0svR2s2SU0ra2FibUY0d0lEQVFBQm8yRXdYekFPQmdOVkhROEJBZjhFCkJBTUNBcVF3SFFZRFZSMGxCQll3RkFZSUt3WUJCUVVIQXdFR0NDc0dBUVVGQndNQ01BOEdBMVVkRXdFQi93UUYKTUFNQkFmOHdIUVlEVlIwT0JCWUVGRk4vYkhTS256ZE9IZ0k4d2ttNlpPbnV0eTRxTUEwR0NTcUdTSWIzRFFFQgpDd1VBQTRJQkFRQXRxNk9vRDI2NWF4Y2x3QVg3ZzdTdEtiZFNkeVNNcC9GbEJZOEJTS0QzdUxDWUtJZmRMdnJJClhKa0Z6MUFXa3hLb1dDbyt2RFl2cEUybE42WXAvakRQZUhZd1c3WG1HQTZJZDRVZ2FtdzV2NHhVZXg5Wis0V1IKbzdqNnV1NkVYK0xOdkQzREFSOFk4aEN3S1NDV3JNWURGbWV3Wmh6N05kY1VBcEp5M3phWTZWeHMvS3dlTGxicwpwbHh2TjlIWCtocVZobC8rWkFtbFZOOVZmZkhHblpsZm5tZW5Tb3RSbjJnR3Rmc0VrV3dhR3UvOUNPbTNQZlhTCjNTY0NGZTNNSjBZbjYvcG1iQkFVVnRtRjFUOTNsT2FYZ3VIek1pWEhJdyt4NUhadnhidkRQbmZ0Z0tnQWpWWU0KRmY0ODlRb28yalVuRVNmK2JRZFczcnpjMUFaMndwbmgKLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQo="
 )
 
 var (
@@ -112,6 +116,50 @@ func TestGenerateRef(t *testing.T) {
 			assert.NotNil(t, err)
 		} else {
 			assert.Contains(t, ref.Name(), tc.expectedRefName)
+			assert.NoError(t, err)
+		}
+	}
+}
+
+func TestSetupTransportOpts(t *testing.T) {
+	// create table test for the following function
+	// func setupTransportOpts(opts []remote.Option, caCert string) ([]remote.Option, error)
+
+	type testCase struct {
+		inputOpts    []remote.Option
+		caCert       string
+		expectedOpts []remote.Option
+		expectErr    bool
+	}
+
+	testCases := []testCase{
+		{
+			inputOpts:    []remote.Option{},
+			caCert:       "",
+			expectedOpts: []remote.Option{},
+			expectErr:    false,
+		},
+		{
+			inputOpts:    []remote.Option{},
+			caCert:       caCert,
+			expectedOpts: []remote.Option{remote.WithTransport(&http.Transport{})},
+			expectErr:    false,
+		},
+		{
+			inputOpts:    []remote.Option{},
+			caCert:       "invalid cert",
+			expectedOpts: []remote.Option{},
+			expectErr:    true,
+		},
+	}
+
+	for _, tc := range testCases {
+		opts, err := setupTransportOpts(tc.inputOpts, tc.caCert)
+
+		if tc.expectErr {
+			assert.NotNil(t, err)
+		} else {
+			assert.Equal(t, len(tc.expectedOpts), len(opts))
 			assert.NoError(t, err)
 		}
 	}
