a72bbef484cbfc1f29c1d13b1458d37376f66e13
Ahmad Ibrahim <ahmad.ibrahim@spectrocloud.com>
2024-02-23 16:34:24 -0800

    test: add TestNewCosignVerifier test

--- internal/verifier/verifier_test.go
+++ internal/verifier/verifier_test.go
@@ -1,6 +1,8 @@
 package verifier
 
 import (
+	"context"
+	"errors"
 	"net/http"
 	"reflect"
 	"testing"
@@ -82,6 +84,53 @@ func TestOptions(t *testing.T) {
 				if len(o.ROpt) != 0 {
 					t.Errorf("got %d remote options, want %d", len(o.ROpt), 0)
 				}
+			}
+		})
+	}
+}
+
+func TestNewCosignVerifier(t *testing.T) {
+	testCases := []struct {
+		name        string
+		publicKey   []byte
+		remoteOpts  []remote.Option
+		expectError error
+	}{
+		{
+			name: "Successful initialization with public key and remote options",
+			publicKey: []byte(`-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEKPuCo9AmJCpqGWhefjbhkFcr1GA3
+iNa765seE3jYC3MGUe5h52393Dhy7B5bXGsg6EfPpNYamlAEWjxCpHF3Lg==
+-----END PUBLIC KEY-----`),
+			remoteOpts:  []remote.Option{remote.WithAuthFromKeychain(authn.DefaultKeychain)},
+			expectError: nil,
+		},
+		{
+			name:        "Failure due to missing public key",
+			publicKey:   nil,
+			remoteOpts:  nil,
+			expectError: errors.New("public key required for cosign verifier"),
+		},
+		{
+			name:        "Failure due to invalid public key",
+			publicKey:   []byte("invalid public key"),
+			remoteOpts:  nil,
+			expectError: errors.New("PEM decoding failed"),
+		},
+	}
+
+	ctx := context.Background()
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := NewCosignVerifier(ctx, WithPublicKey(tc.publicKey), WithRemoteOptions(tc.remoteOpts...))
+
+			if err == nil && tc.expectError != nil {
+				t.Errorf("Expected error for %s, got nil", tc.name)
+			} else if err != nil && tc.expectError == nil {
+				t.Errorf("Unexpected error for %s: %v", tc.name, err)
+			} else if err != nil && tc.expectError != nil && err.Error() != tc.expectError.Error() {
+				t.Errorf("Expected error '%v', got '%v'", tc.expectError, err)
 			}
 		})
 	}
